eb0ce5c1025edf7ee4960efbe1477391eed2015e
zahra_f <73747335+zahrafarrokhi@users.noreply.github.com>
2022-07-05 11:30:42 +0430

    Update index.js

--- mysite_frontend-main/src/index.js
+++ mysite_frontend-main/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 ReactDOM.render(
@@ -10,3 +11,4 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
