e164658b2dad9d62795c9117a65c3f6225bf9572
yasi <zahrafarrokhi2017@gmail.com>
2022-07-14 19:30:51 +0430

    fix

--- 6-useContext/src/components/ProductItem/ProductItem.jsx
+++ 6-useContext/src/components/ProductItem/ProductItem.jsx
@@ -1,64 +1,39 @@
-import React, { useContext } from 'react'
+import React, { useContext } from "react";
 import "./ProductItem.css";
-import Image from '../Image/Image'
+import Image from "../Image/Image";
 import { Link } from "react-router-dom";
 import Button from "../Button/Button";
 import ThemeContext from "../../contexts/ThemeContext";
 
-// const ProductItem = ({ data }) => (
-//   const themeValues = useContext(ThemeContext);
-//   console.log("themeValues",themeValues)
-//   return(
-//   <Link to={`/product/${data.id}`}>
-//   <li className="ProductItem visible">
-//     <h3>{data.name}</h3>
-  
-//     {/* props */}
-//     <Image imgSrc={data.image} style={{ width: "200px", height: "200px" }} alt="productpic"/> 
-//     <span>Price:{data.price}</span>
-   
-//     {/* props */}
-//     {/* <Button text='click here' /> */}
-
-//     {/* children */}
-//     <Button>Add to cart</Button>
-//   </li>
-//   </Link>)
-  
-// );
-
-// export default ProductItem;
-
-
-
 const ProductItem = ({ data }) => {
   const themeValues = useContext(ThemeContext);
-  console.log(themeValues)
-
-
+  console.log(themeValues);
 
   return (
-    <li className="ProductItem" >
+    <li className="ProductItem">
       <Link to={`/product/${data.id}`}>
         <h3>{data.name}</h3>
       </Link>
-      <Image imgSrc={data.image} style={{ width: "200px", height: "200px" }} alt="productpic"/> 
+      <Image
+        imgSrc={data.image}
+        style={{ width: "200px", height: "200px" }}
+        alt="productpic"
+      />
 
-      
       <span>Price: {data.price}</span>
       <Button
-       style={{
-        color: themeValues.theme.color,
-        borderColor: themeValues.theme.color,
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-      }}
+        style={{
+          color: themeValues.theme.color,
+          borderColor: themeValues.theme.color,
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
       >
-       Add to cart
+        Add to cart
       </Button>
     </li>
   );
 };
 
-export default ProductItem
+export default ProductItem;
