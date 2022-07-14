7949fb9b41a4017f990d8705c1106b8b3591a17c
yasi <zahrafarrokhi2017@gmail.com>
2022-07-14 19:54:16 +0430

    fix

--- 6-useContext/src/components/App/App.js
+++ 6-useContext/src/components/App/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import React, { useState } from "react";
 import { Outlet } from "react-router-dom";
 import Layout from "../Layout/Layout";
+//theme
 import ThemeContext from "../../contexts/ThemeContext";
 import themeConfig from "../../configs/theme";
 
